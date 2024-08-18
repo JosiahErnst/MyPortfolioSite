adcca85d4b946d1d91c74274b4d0cd2fe0af5127
JosiahErnst <josiah.ernst.2003@gmail.com>
2024-08-18 17:20:12 -0500

    adding base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'JEPortfolioSite';
+  title = 'Josiah Ernst - Software Engineer';
 }
